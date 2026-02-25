2231c6e4fe0575bab31afb40527584a5a972e858
Andrew Huebner <ahuebne6@depaul.edu>
2026-02-24 21:58:00 -0600

    Refactor main method and update output method
    Updated main method signature to public and added args parameter. Replaced IO.println with System.out.println for output.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,13 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
+        // Use System.out instead of IO
+        System.out.println(String.format("Hello and welcome!"));
+    
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+       // IO.println(String.format("Hello and welcome!"));
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
